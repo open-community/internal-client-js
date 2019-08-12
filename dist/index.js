71720f2fc9f4614db2e7839b186ea05781db4215
Abraham Tewa <abraham.tewa@sewan.fr>
2019-08-12 16:26:50 +0200

    Fix bug

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var _exportNames = {};
+var _exportNames = {
+  errors: true
+};
 Object.defineProperty(exports, "default", {
   enumerable: true,
   get: function () {
     return _Client.default;
   }
 });
+exports.errors = void 0;
+
+var httpErrors = _interopRequireWildcard(require("./httpErrors"));
 
 var _constants = require("./constants");
 
@@ -27,4 +32,11 @@ Object.keys(_constants).forEach(function (key) {
 var _Client = _interopRequireDefault(require("./Client"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+const errors = {
+  http: httpErrors
+};
+exports.errors = errors;
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import * as httpErrors from './httpErrors';
+
 export * from './constants';
 
 export { default } from './Client';
+
+const errors = {
+    http: httpErrors,
+};
+
+export { errors };
